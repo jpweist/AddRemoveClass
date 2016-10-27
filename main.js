6e08eae6f2ff162c687a6f140c66d7982937d3f0
jpweist <jp_weist@yahoo.com>
2016-10-26 15:01:12 -1000

    i don’t understand why this will not work. help

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById("#buttonAdd").addEventListener("click", function() {
+  document.getElementById("#text").classList.add("classText");
+});
+
+document.getElementById("#buttonRemove").addEventListener("click", function() {
+  document.getElementById("#text").classList.remove("classText");
+});
